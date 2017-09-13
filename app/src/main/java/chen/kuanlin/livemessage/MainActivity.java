e00c4fe5082b56acb9bfb1f7c80f93d992ed0631
Kuanlin-Chen <webb1992810625@gmail.com>
2017-09-13 11:35:29 +0800

    Remove unnecessary module

--- app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
+++ app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
@@ -2,15 +2,9 @@ package chen.kuanlin.livemessage;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -20,14 +14,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/chen/kuanlin/livemessage/Recorder.java
+++ app/src/main/java/chen/kuanlin/livemessage/Recorder.java
@@ -14,7 +14,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
