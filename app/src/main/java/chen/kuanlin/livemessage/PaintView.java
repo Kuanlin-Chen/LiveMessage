d72088f7515869dab31f6c2f064b043e9f41e514
Kuanlin-Chen <webb1992810625@gmail.com>
2017-09-13 11:28:41 +0800

    Implement Rect in PaintView

--- app/src/main/java/chen/kuanlin/livemessage/PaintView.java
+++ app/src/main/java/chen/kuanlin/livemessage/PaintView.java
@@ -6,38 +6,34 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.icu.text.SimpleDateFormat;
-import android.os.Environment;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-
 /**
  * Created by kuanlin on 2017/9/7.
  */
 
-public class PaintView extends View {
+public class PaintView extends View{
 
-    private float mov_x;
-    private float mov_y;
+    private int StylePenID = -1;
+    private float pre_x;
+    private float pre_y;
+    private float mCurveEndX;
+    private float mCurveEndY;
     private static boolean isFirstPenTouch = false;
     private static boolean isMoving = false;
-    private int StylePenID = -1;
+
     private Paint paint;
     private Canvas canvas;
     private Bitmap bitmap;
     private Path mPath = new Path();
+    private Rect mInvalidRect = new Rect();
+
+    private String TAG = "[PaintView] ";
     private boolean debugmode = false;
-    private final String TAG = "[PaintView] ";
 
     public PaintView(Context context) {
         super(context);
@@ -63,13 +59,16 @@ public class PaintView extends View {
     private void initPaintView() {
         paint = new Paint(Paint.DITHER_FLAG);
         bitmap = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Log.e(TAG,"Width:"+String.valueOf(getMeasuredWidth())+" Height:"+String.valueOf(getMeasuredHeight()));
+        if(debugmode)Log.e(TAG,"Width:"+String.valueOf(getMeasuredWidth())+" Height:"+String.valueOf(getMeasuredHeight()));
         canvas = new Canvas();
         canvas.setBitmap(bitmap);
 
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(5);
         paint.setColor(Color.BLACK);
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setStrokeWidth((float) 3.0);
     }
 
     @Override
@@ -79,12 +78,8 @@ public class PaintView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        //float paintsize = event.getSize();
         int index = event.getActionIndex();
         int ID = event.getPointerId(index);
-
-        paint.setColor(Color.WHITE);
-        paint.setStyle(Paint.Style.FILL);
 
         if(debugmode){
             Log.e(TAG,"index:" + index );
@@ -96,24 +91,15 @@ public class PaintView extends View {
         {
             case MotionEvent.ACTION_MOVE:
                 if(debugmode) Log.e(TAG,"Action Move 2" + event.getX() + "/" + event.getY());
-                paint.setColor(Color.RED);
-                paint.setAntiAlias(true);
-                paint.setDither(true);
-                paint.setStrokeWidth((float) 3.0);
-                paint.setStyle(Paint.Style.STROKE);
 
                 if((ID == StylePenID)) {
                     isMoving = true;
-                    float x = event.getX();
-                    float y = event.getY();
-                    float cX = (x + mov_x) / 2;
-                    float cY = (y + mov_y) / 2;
-                    mPath.quadTo(cX,cY,x,y);
-                    canvas.drawPath(mPath, paint);
-                    mov_x = x;
-                    mov_y = y;
+                    Rect rect = RectMove(event);
+                    if (rect != null) {
+                        invalidate(rect);
+                    }
+                    invalidate();
                 }
-                invalidate();
                 break;
 
             case MotionEvent.ACTION_POINTER_DOWN:
@@ -128,8 +114,8 @@ public class PaintView extends View {
                     StylePenID = ID;
                 }
                 if ( ( isFirstPenTouch == true ) && (ID == StylePenID) ) {
-                    mov_x = (int) event.getX();
-                    mov_y = (int) event.getY();
+                    pre_x = (int) event.getX();
+                    pre_y = (int) event.getY();
                     mPath.moveTo( event.getX(), event.getY());
                     //canvas.drawPoint(mov_x, mov_y, paint);
                 }
@@ -163,5 +149,52 @@ public class PaintView extends View {
                 break;
         }
         return true;
+    }
+
+    private Rect RectMove(MotionEvent event) {
+        Rect areaToRefresh = null;
+
+        final float x = event.getX();
+        final float y = event.getY();
+
+        final float previousX = pre_x;
+        final float previousY = pre_y;
+
+        final float dx = Math.abs(x - previousX);
+        final float dy = Math.abs(y - previousY);
+
+        if (dx >= 3 || dy >= 3) {
+            areaToRefresh = mInvalidRect;
+            areaToRefresh.set((int) mCurveEndX, (int) mCurveEndY,
+                    (int) mCurveEndX, (int) mCurveEndY);
+
+            //設定貝賽爾曲線的操作點為起點和終點的一半
+            float cX = mCurveEndX = (x + previousX) / 2;
+            float cY = mCurveEndY = (y + previousY) / 2;
+
+            //實現繪製貝賽爾平滑曲線；previousX, previousY為操作點，cX, cY為終點
+            mPath.quadTo(previousX, previousY, cX, cY);
+            //mPath.lineTo(x, y);
+
+            // union with the control point of the new curve
+            //areaToRefresh矩形擴大了border(寬和高擴大了兩倍border)，
+            //border值由設定手勢畫筆粗細值决定
+
+            areaToRefresh.union((int) previousX, (int) previousY,
+                    (int) previousX, (int) previousY);
+            /* areaToRefresh.union((int) x, (int) y,
+                    (int) x, (int) y);*/
+
+
+            // union with the end point of the new curve
+            areaToRefresh.union((int) cX, (int) cY,
+                    (int) cX, (int) cY);
+
+            //第二次執行時，第一次结束調用的座標值將作為第二次調用的初始坐標值
+            pre_x = x;
+            pre_y = y;
+            canvas.drawPath(mPath, paint);
+        }
+        return areaToRefresh;
     }
 }
