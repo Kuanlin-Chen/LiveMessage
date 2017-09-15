5c95f6a9fc9dc675b3c4be3e2869a2ed30c9f798
Kuanlin-Chen <webb1992810625@gmail.com>
2017-09-15 14:07:48 +0800

    1) Add gifflen source code into jni folder 2) Include gifflen module in build.gradle 3) Modify the gifflen.cpp to match my package name 4) Load library and declare native method in MainActivity

--- app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
+++ app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
@@ -32,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
     private boolean debugmode = true;
     private final String TAG = "[MainActivity] ";
 
+    static {
+        try{
+            System.loadLibrary("gifflen");
+        }catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public native int Init(String gifName, int w, int h, int numColors, int quality, int frameDelay);
+    public native void Close();
+    public native int AddFrame(int[] inArray);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
