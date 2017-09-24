de331f448cccd2d15868bcb6e42f2665b7eaaa80
Kuanlin-Chen <webb1992810625@gmail.com>
2017-09-24 16:40:11 +0800

    1) Add menu_main.xml 2) Set up onCreateOptionsMenu() and onOptionsItemSelected()

--- app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
+++ app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -352,5 +355,33 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id){
+            case R.id.menu_english:
+                Toast.makeText(MainActivity.this,"English",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_chinese:
+                Toast.makeText(MainActivity.this,"Chinese",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
