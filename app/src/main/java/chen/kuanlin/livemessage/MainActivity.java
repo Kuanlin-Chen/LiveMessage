311580cf1165cb371d6565ed8c76c7ad904029f3
Kuanlin-Chen <webb1992810625@gmail.com>
2017-09-26 16:19:42 +0800

    1) Add res/menu/menu_main.xml 2) Add the function of OptionsMenu

--- app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
+++ app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -349,6 +352,32 @@ public class MainActivity extends AppCompatActivity {
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.menu_guide:
+                QuickGuide quickGuide = new QuickGuide(MainActivity.this);
+                quickGuide.showQuickGuide();
+                break;
+            case R.id.menu_feedback:
+                Toast.makeText(MainActivity.this,"Feedback",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
