c9aeb4ebf5bd9b2df74c42ffadc65da11e198d2c
Kuanlin-Chen <webb1992810625@gmail.com>
2017-11-17 12:07:22 +0800

    1) Refactor the code 2) Modify versionCode and versionName

--- app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
+++ app/src/main/java/chen/kuanlin/livemessage/MainActivity.java
@@ -71,9 +71,9 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
 
-        if(mySharedPreference.getUserVersion()!=6){
+        if(mySharedPreference.getUserVersion()!=7){
             setMySharedPreference(); //initialize preference
-            mySharedPreference.saveUserVersion(6);
+            mySharedPreference.saveUserVersion(7);
             if(mySharedPreference.getGuide()){
                 //UpdateGuide updateGuide = new UpdateGuide(MainActivity.this);
                 //updateGuide.showUpdateGuide();
