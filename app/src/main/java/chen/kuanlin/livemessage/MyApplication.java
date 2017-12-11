33bdcc33655020a8d698bdbb1a9c60a86a504e14
Kuanlin-Chen <webb1992810625@gmail.com>
2017-12-11 21:26:50 +0800

    1) Add the values of String 2) Modify crash reporter

--- app/src/main/java/chen/kuanlin/livemessage/MyApplication.java
+++ app/src/main/java/chen/kuanlin/livemessage/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 /**
@@ -11,7 +12,11 @@ import org.acra.annotation.ReportsCrashes;
  */
 
 @ReportsCrashes(
-        mailTo = "kuanlin81625＠gmail.com"
+        mailTo = "kuanlin81625@gmail.com",
+        mode = ReportingInteractionMode.DIALOG,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogTitle = R.string.crash_dialog_title,
+        logcatArguments = { "-t", "200", "-v", "time" }
 )
 public class MyApplication extends Application {
     @Override
